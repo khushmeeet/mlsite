45116d78e2c2bca00c6ceeb3c7af615350c7195b
Khushmeet <khushmeetsingh1996@gmail.com>
2017-06-16 14:54:19 +0530

    new lstm model

--- load.py
+++ load.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 def init_model():
     perceptron_model = load_model('3layer.h5')
-    lstm_model = load_model('lstm2.h5')
+    lstm_model = load_model('lstm.h5')
     perceptron_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     lstm_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     graph = tf.get_default_graph()
